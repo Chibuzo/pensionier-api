d94f358005e91f16df5153380faccc46bc93c68e
Chibuzo <chibuzo.henry@gmail.com>
2022-05-27 09:39:42 +0100

    add user route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const routes = require('express').Router();
 const personRoutes = require('./person');
+const userRoutes = require('./user');
+
 
 routes.get('/', (req, res) => {
     res.status(200).json({ message: 'What are you looking for here??!!' });
 });
 
 routes.use('/persons', personRoutes);
+
+routes.use('/users', userRoutes);
 
 
 module.exports = routes;
